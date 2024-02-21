6989c3552f9f6eb6c7d411b884574a31f59ecd37
Pham Tuan Kiet <131690921+thekane02@users.noreply.github.com>
2024-02-21 22:16:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import "./css/Todo.css";
 
 const STORAGE_KEY = "todoList";
 
+const filterByStatus = (listTodos = [], status = '', id) => {
+  switch (status) {
+    case 'ACTIVE':
+      return listTodos.filter(item => !item.isCompleted)
+    case 'COMPLETED':
+      return listTodos.filter(item => item.isCompleted)
+    case 'REMOVE':
+      return listTodos.filter(item => item.id !== id)
+    default:
+      return listTodos
+  }
+}
 // const filterTodosLeft = (listTodos = []) => {
 //   return listTodos.filter((item) => !item.isCompleted);
 // };
